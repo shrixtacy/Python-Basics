932300a3fe6bb5d1464a64d5b8f659e4453d4c88
shrixtacy <shriyanshdash12@gmail.com>
2025-12-16 20:21:16 +0530

    Initial commit

---
+++ 1-main.py
@@ -0,0 +1,11 @@
+#This is my first lines of code in python 
+#print statement is basically used to execute the lines of codes. 
+print("I love Biriyani")
+print("Its really good")
+print("Fuck off")
+
+#This is also correct but wierd way to write the print statement
+print(
+    "Woow.Lol"
+)
+
